d2df8b8b0f3e04e4dfb092972069e2cf4f7fb26f
yeseung-2 <kysv0202@gmail.com>
2026-07-01 00:30:55 +0900

    fix base url

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -9,11 +10,18 @@ app = FastAPI(
     version="0.1.0",
 )
 
+frontend_url = os.getenv("FRONTEND_URL")
+
+allow_origins = [
+    "http://localhost:3000",
+]
+
+if frontend_url:
+    allow_origins.append(frontend_url)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-    ],
+    allow_origins=allow_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
