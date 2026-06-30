e3ddcd2c1b916980d47b0ea95258d98c6ad33211
yeseung-2 <kysv0202@gmail.com>
2026-07-01 00:35:49 +0900

    fix base url 2

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  timeout: 10000,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,
 });
